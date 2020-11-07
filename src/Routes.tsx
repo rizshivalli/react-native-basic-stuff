add388b0054845dee4af6db37c860ff7701f2ade
Rizwan Shivalli <rizwan.a@simplificommerce.com>
2020-11-07 22:33:53 +0530

    easy imports configured

---
+++ src/Routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Login from '@Screens/Login';
+
+interface RoutesProps {}
+
+const Stack = createStackNavigator();
+
+const Routes: React.FC<RoutesProps> = ({}) => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Stack" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
